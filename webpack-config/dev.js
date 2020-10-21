87c3180476765fafcb41c3c83cc6e987375301e7
theajack <1506282385@qq.com>
2020-10-21 22:11:14 +0800

    feat: initial

---
+++ webpack-config/dev.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 分离css
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve('./', 'public/main.js'),
+    output: {
+        path: path.resolve('./', 'public'),
+        filename: 'bundle.js'
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: path.resolve('./', 'public'),
+        historyApiFallback: true,
+        inline: true,
+        host: 'localhost'// '0.0.0.0' //
+    },
+    module: {
+        rules: [{
+            test: /(.js)$/,
+            use: [{
+                loader: 'babel-loader',
+            }]
+        }, {
+            enforce: 'pre',
+            test: /\.js$/,
+            loader: 'eslint-loader',
+            exclude: /node_modules/,
+            options: {
+                configFile: './.eslintrc.js'
+            }
+        }, {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        }, {
+            test: /\.less$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+        }, {
+            test: /\.(png|jpg|gif|svg|woff2?|eot|ttf)(\?.*)?$/,
+            loader: 'url-loader',
+            options: {
+                limit: 50000,
+            },
+        }, {
+            test: /\.html$/,
+            loader: 'html-loader',
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/[name].min.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './helper/index.tpl.html',
+            filename: 'index.html',
+        }),
+        new OptimizeCssAssetsPlugin()
+    ]
+};
